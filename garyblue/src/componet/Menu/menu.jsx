33d466c4da747dfa7f8fcc76d7b67c77bd04f88b
Dominic Whelan <dominic.whelan27@gmail.com>
2022-04-13 09:20:21 -0230

    Menu Pics added

--- garyblue/src/componet/Menu/menu.jsx
+++ garyblue/src/componet/Menu/menu.jsx
@@ -1,9 +1,37 @@
 import React from "react";
+import Pic1 from "../../Images/Server.avif";
+import Pic2 from "../../Images/menu.jpg";
 
 const Menu = () => {
   return (
     <div>
-      <h1>Menus</h1>
+      <h1 style={{ marginTop: "2rem" }}>
+        <span>
+          <img
+            src={Pic1}
+            alt="Server Pic"
+            width="400rem"
+            height="132rem"
+            class="shadow me-5"
+            style={{
+              borderRadius: "1rem",
+            }}
+          />
+        </span>
+        Our Menu
+        <span>
+          <img
+            src={Pic2}
+            alt="Menu Pic"
+            width="400rem"
+            height="132rem"
+            class="shadow ml-5"
+            style={{
+              borderRadius: "1rem",
+            }}
+          />
+        </span>
+      </h1>
     </div>
   );
 };
